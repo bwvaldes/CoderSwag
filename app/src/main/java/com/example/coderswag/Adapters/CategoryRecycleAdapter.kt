9cb12d4028b098e5127b4fb27d861ec37b2f85c1
Brian <brianwvaldes@gmail.com>
2018-10-03 17:53:19 -0700

    passing intent and recycler views

--- app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -12,28 +12,29 @@ import com.example.coderswag.R
 import kotlinx.android.synthetic.main.category_list_item.view.*
 import java.util.zip.Inflater
 
-class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>, val itemClick: (Category) -> Unit) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
     override fun onBindViewHolder(p0: Holder, p1: Int) {
         p0?.bindCategory(categories[p1], context)
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.category_list_item, p0, false)
-        return Holder(view)    }
+        return Holder(view, itemClick)    }
 
     override fun getItemCount(): Int {
         return categories.count()
     }
 
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
-        val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
+    inner class Holder(itemView: View, val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryName)
 
         fun bindCategory(category: Category, context: Context) {
             val resourceId = context.resources.getIdentifier(category.image,
                     "drawable", context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text = category.title
+            itemView.setOnClickListener { itemClick(category) }
         }
 
     }
--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.coderswag.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -9,6 +10,7 @@ import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataServices
+import com.example.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +21,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this, DataServices.categories)
+        adapter = CategoryRecycleAdapter(this, DataServices.categories) {
+            category -> println(category.title)
+            val productIntent = Intent(this, ProductActivity:: class.java)
+            productIntent.putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
+
+        }
         categoryListView.adapter = adapter
 
         val layoutManager = LinearLayoutManager(this)
         categoryListView.layoutManager = layoutManager
         categoryListView.setHasFixedSize(true)
     }
+
 
 }
